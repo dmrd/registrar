272a73ad25ebb79e3f783a8b5dc2a31a69ff4b20
Ubuntu <ubuntu@ip-10-244-30-215.ec2.internal>
2013-04-14 07:40:55 +0000

    Functioning program.  Pulls to mongodb

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 registrar
 =========
 
-Track Princeton course enrollment over time
+Track Princeton course enrollment over time.  Polls the registrar to gather data, enter in local mongodb database.
+
+Usage:
+Control semester (spring/fall 2013) with first argument.
+python poll.py spring
+python poll.py fall
--- poll.py
+++ poll.py
@@ -1,5 +1,6 @@
-#Poll
+#Poll the registrar website
 #Based on printer polling script from Shubhro Saha
+import sys
 import datetime
 import urllib, datetime, time
 from bs4 import BeautifulSoup
@@ -7,17 +8,39 @@ from bs4 import BeautifulSoup
 import pymongo
 from pymongo import MongoClient
 
+#Pause times in changes
+minPause = 10
+maxPause = 900
+timeMultiplier = 2
+
+
+print("Connecting to database...")
 #Establish database connection
 connection = MongoClient()
 courseDB = connection.courses
-courses = courseDB.fall13
+print("Connection successful")
+
+#Fall 2013 url
+if (sys.argv[1] == "fall"):
+    CourseUrl = "http://registrar.princeton.edu/course-offerings/search_results.xml?submit=Search&term=1142"
+    courses = courseDB.fall13
+    print("Polling fall 2013 courses...")
+elif (sys.argv[1] == "spring"):
+    CourseUrl = "http://registrar.princeton.edu/course-offerings/search_results.xml?submit=Search&term=1134"
+    courses = courseDB.spring13
+    print("Polling spring 2013 courses...")
+else:
+    print("Invalid semester")
+    exit()
+
 
 #Columns ordered as:
 #class number #; DEP ###; title; distribution; Section; Days; Time;
 #                   Location; Enrollment; Max enrollment; Status; Books; Eval
 
 def addClass(datetime,line):
-    if (len(line) != 13): return
+    if (len(line) != 13): return False
+    print("-Entering class {0} into database".format(line[0]))
     entry = {
             'number' : line[0],
             'course' : line[1],
@@ -31,44 +54,57 @@ def addClass(datetime,line):
             'max' : [(datetime,line[9])],  #Does max enrollment change over time?
             'Status' : [(datetime,line[10])]  #Does max enrollment change over time?
             }
-    courseDB.insert(entry)
-    return
+    courses.insert(entry)
+    return True
 
 #Note changed enrollment numbers or add new classes.  Assume only enrollment
 # changes
 def updateClass(datetime,line):
-    if (len(line) != 13): return
-    entry = courseDB.find_one({'number' : line[0]})
+    if (len(line) != 13): return False
+    updateFlag = False
+    entry = courses.find_one({'number' : line[0]})
     if (entry == None):
-        addClass(datetime, line)
+        updateFlag = addClass(datetime, line)
     else:
         if (entry["enrollment"][-1][1] != line[8]):
             entry["enrollment"].append((datetime,line[8]))
+            updateFlag = True
         if (entry["max"][-1][1] != line[9]):
             entry["max"].append((datetime,line[9]))
+            updateFlag = True
         if (entry["Status"][-1][1] != line[10]):
             entry["Status"].append((datetime,line[10]))
+            updateFlag = True
         courses.save(entry)
-    return
+    return updateFlag
 
-#Fall 2013 url
-CourseUrl = "http://registrar.princeton.edu/course-offerings/search_results.xml?submit=Search&term=1142"
 
+print("Entering polling loop...")
+pauseTime = minPause
 while True:
+    updateFlag = False
+    changes = 0
     now = datetime.datetime.now()
-    #f = urllib.urlopen(CourseUrl)
-    f = open("reg.html")
+    print(now)
+    f = urllib.urlopen(CourseUrl)
+    #f = open("reg.html")
     html = f.read()
 
     soup = BeautifulSoup(html)
 
     #Go over every table row
     tr_list = soup.find_all('tr')
-    for tr in tr_list[]:
+    for tr in tr_list:
         columns = tr.find_all('td')
         #Construct list of columns for this row
         colText = [entry.get_text(" ",strip=True).replace("\n","") for entry in columns]
-        updateClass(now, colText)
-
-    print("Pausing for 10 minutes")
-    time.sleep(60)
+	change  = updateClass(now,colText)
+	if (change): #Count changes
+            changes += 1
+        updateFlag |= change #Has there been any change at all this loop?
+    if (updateFlag): #Reset update period 
+        pauseTime = minPause
+    else: #No changes.  Pause for longer time (or max time, whichever is smaller)
+        pauseTime = min(maxPause, pauseTime * timeMultiplier)
+    print("Pausing for {0} seconds after {1} changes".format(pauseTime, changes))
+    time.sleep(pauseTime)
